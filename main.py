5d0c686b23124953f53272bd626578ff36a60866
Christopher De Vries <devries@idolstarastronomer.com>
2013-05-13 15:08:19 -0700

    Added color touch fading on the serpent body, serpent segment indicators, head tail indicators.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
 import comcycle
 
+class FingerTouch(Widget):
+    def fade(self):
+        self.opacity = self.opacity*0.8
+
 class SerpentUI(FloatLayout):
+    finger = ObjectProperty(None)
+
     def do_touch(self,touch):
         touch.apply_transform_2d(self.to_local)
         xmin,ymin = self.serpent.pos
@@ -15,6 +23,8 @@ class SerpentUI(FloatLayout):
         if xf>=0.0 and yf>=0.0 and xf<1.0 and yf<1.0:
             self.pattern.touch_point(1.0-yf,xf)
             print "Area touched: %f,%f"%(xf,yf)
+            self.finger.center = touch.pos
+            self.finger.opacity=1.0
 
     def do_press(self):
         ipaddr = '192.168.10.88'
@@ -34,10 +44,15 @@ class SerpentUI(FloatLayout):
     def set_blue(self,blue):
         self.pattern.blue=blue
 
+    def periodic(self,dt):
+        self.finger.fade()
+
+
 class SerpentApp(App):
     def build(self):
         s = SerpentUI()
         s.pattern = comcycle.BillowPattern()
+        Clock.schedule_interval(s.periodic,0.1)
         return s
 
 if __name__=='__main__':
